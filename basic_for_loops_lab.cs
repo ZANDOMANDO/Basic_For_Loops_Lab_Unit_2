00a479a117df854bf16e697078c0d008181a7af3
Eddy <eduardo.di.garcia@gmail.com>
2022-07-06 17:50:47 -0400

    Unit 2 basic for loops lab yeay :)

---
+++ basic_for_loops_lab.cs
@@ -0,0 +1,22 @@
+﻿bool continueLoop;
+do
+{
+    Console.WriteLine("Enter a number.");
+    int num = int.Parse(Console.ReadLine());
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine(sum);
+    Console.WriteLine("Would you like to continue (y/n)?");
+    if (Console.ReadLine().Equals("y")) {
+        continueLoop = true;
+    }
+    else
+    {
+        continueLoop = false;
+    }
+} while (continueLoop);
+
+Console.WriteLine("Goodbye!");
